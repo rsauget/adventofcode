cab0d20834f9467c295d870030d412d9e21184c7
Raphaël Sauget <raphael@georges.tech>
2022-12-11 16:27:54 +0100

    day11

--- src/advent_clj/core.clj
+++ src/advent_clj/core.clj
@@ -9,7 +9,8 @@
             [advent-clj.day7 :as day7]
             [advent-clj.day8 :as day8]
             [advent-clj.day9 :as day9]
-            [advent-clj.day10 :as day10]))
+            [advent-clj.day10 :as day10]
+            [advent-clj.day11 :as day11]))
 
 (defn -main
   []
@@ -22,4 +23,5 @@
   (day7/day7)
   (day8/day8)
   (day9/day9)
-  (day10/day10))
+  (day10/day10)
+  (day11/day11))
